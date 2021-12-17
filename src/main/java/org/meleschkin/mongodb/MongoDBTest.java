d0fc0a210546621de70cfbe7903e9f12bb06f1d3
Andrei Meleschkin <meleschkin@gmail.com>
2021-12-17 19:31:01 +0100

    MongoDBTest

--- src/main/java/org/meleschkin/mongodb/MongoDBTest.java
+++ src/main/java/org/meleschkin/mongodb/MongoDBTest.java
@@ -15,7 +15,9 @@ import org.bson.json.JsonWriterSettings;
 import org.meleschkin.config.Configuration;
 import org.meleschkin.config.MyConfigurator;
 
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 @Log4j2
 public class MongoDBTest {
@@ -43,8 +45,18 @@ public class MongoDBTest {
                     log.info(mc.getUserName());
                     log.info(mc.getPassword());
                 }
+                log.info("Databases");
+                List<String> databases = mongoClient.listDatabaseNames().into(new ArrayList<>());
+                for (String db : databases) {
+                    log.info(db);
+                }
                 MongoDatabase database = mongoClient.getDatabase(config.getDatabase());
                 log.info(database.getName());
+                log.info("Collections in " + database.getName());
+                List<String> collections = database.listCollectionNames().into(new ArrayList<>());
+                for (String col : collections) {
+                    log.info(col);
+                }
                 MongoCollection<Document> collection = database.getCollection(config.getCollection());
                 MongoNamespace mn = collection.getNamespace();
                 log.info(mn.toString());
